300c21680a67febb8af706b8822c6f727d2032d6
noViceMin <admin@isamin.kr>
2023-12-15 02:31:23 +0000

    Update setting.py

--- app/setting.py
+++ app/setting.py
@@ -2,6 +2,7 @@ import os
 
 MYSQL_USER = os.environ['MYSQL_USER']
 MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
+MYSQL_HOST = os.environ['MYSQL_HOST']
 MYSQL_PORT = os.environ['MYSQL_PORT']
 MYSQL_DATABASE = os.environ['MYSQL_DATABASE']
 
